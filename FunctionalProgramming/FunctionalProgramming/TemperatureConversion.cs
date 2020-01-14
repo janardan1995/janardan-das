8afb6df5c613e315a4992be205b409efab56cf9e
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-01-14 09:45:15 +0530

    [JD]Add.TEMPERATURE CONVERSION PROGRAM

---
+++ FunctionalProgramming/FunctionalProgramming/TemperatureConversion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgramming
+{
+    class TemperatureConversion
+    {
+        public static void Temperature()
+        {
+            Console.WriteLine("enetr celsius");
+            Double x = Utility.UtiliReadDouble();
+            Console.WriteLine("enetr fahrenheit");
+            Double y = Utility.UtiliReadDouble();
+            Double Fahrenheit = x * (9 / 5) + 32;
+            Double Celsius= (y - 32) * 5 / 9;
+            Console.WriteLine("celsius is: {0} and Fahrenheit is :{1}",Celsius,Fahrenheit);
+
+
+
+        }
+    }
+
+}
