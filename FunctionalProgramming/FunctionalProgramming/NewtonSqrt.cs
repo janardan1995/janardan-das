b74d7ff50df33297ec197d5fb3d9e4d0ae20f5d9
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-01-14 12:29:52 +0530

    [JD]Add.Newton Square root method

---
+++ FunctionalProgramming/FunctionalProgramming/NewtonSqrt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgramming
+{
+    class NewtonSqrt
+    {
+        public static void Newton()
+        {
+            Console.WriteLine("enter the no to perform newton method");
+            Double c = Utility.UtiliReadDouble();
+            Double t = c;
+            Double epsilon =1e-15;
+            while(Math.Abs(t - c / t) > epsilon * t)
+            {
+                t=((c/t)+t)/ 2.0;
+            }
+            Console.WriteLine(t);
+        }
+    }
+}
