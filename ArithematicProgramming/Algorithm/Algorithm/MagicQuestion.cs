12da4650a407da5c48b70e752ed33860519829b8
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-01-18 12:20:20 +0530

    [Jd]Add.primeno with palindrom and magicilly found you number programs

---
+++ ArithematicProgramming/Algorithm/Algorithm/MagicQuestion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class MagicQuestion
+    {
+        public static void Magic()
+        {
+            Console.WriteLine("thing a number from 0 to your choice");
+            Console.WriteLine("type the last boundary as your wish");
+            int x = int.Parse(Console.ReadLine());
+
+            int lowerIndex = 0;
+            int higherIndex = x;
+
+            while (lowerIndex <= higherIndex)
+            {
+
+                int midIndex = ((lowerIndex + higherIndex) / 2)-1;
+
+
+
+                Console.WriteLine("is your no in between " + lowerIndex + " to " + (midIndex-1) + " just type true and false");
+                String b = Console.ReadLine();
+
+                if (b == "T" || b == "t")
+                {
+                    higherIndex = midIndex;
+                }
+                else
+                {
+                    lowerIndex = midIndex;
+                }
+                if (lowerIndex==higherIndex)
+                {
+                    Console.WriteLine("your no is {0}", midIndex);
+                    break;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ ArithematicProgramming/Algorithm/Algorithm/PrimeAnagramPalindrome.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class PrimeAnagramPalindrome
+    {
+        public static void PrimeNo()
+        {
+
+
+            int[] arr = new int[200];
+            //asign 0 to all the element in arr
+            //
+            int k = 0;
+            //
+            for (int i = 2; i < 1000; i++)
+            {
+                int count = 0;
+                for (int j = 1; j <= i; j++)
+                {
+
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    arr[k] = i;
+
+                    k++;
+                }
+            }
+
+
+            Console.WriteLine("numbers" + "........." + "palindrome number");
+            int s = 0;
+            while (arr[s] > 0)
+            {
+                if (arr[s] == Utility.Reverse(arr[s]))
+                {
+
+                    Console.WriteLine(arr[s] + "................" + Utility.Reverse(arr[s]));
+
+                }
+                s++;
+            }
+            Console.WriteLine("__________________________________");
+
+
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+                //for anagram no.
+                int[] arrNo = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                //initially i put 0 to all element
+                arrNo[i] = 0;
+            }
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+
+                    for (int j = i + 1; j < arr.Length; j++)
+                    {
+                        String st1 = arr[i].ToString();
+                        String st2 = arr[j].ToString();
+                        if (st1.Length != st2.Length)
+                        {
+                            break;
+                        }
+                        else
+                        {
+
+                            int x = arr[i];
+                            while (x > 0)
+                            {
+                                int y = x % 10;
+
+                                arrNo[y]++;
+                                x = x / 10;
+                            }
+                            int xy = arr[j];
+
+                            while (xy > 0)
+                            {
+                                int yy = xy % 10;
+
+                                arrNo[yy]--;
+                                xy = xy / 10;
+                            }
+
+                            for (int l = 0; l < 10; l++)
+                            {
+                                if (arrNo[i] != 0)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine(arr[i] + " anagram of " + arr[j]);
+                                }
+                            }
+                        }
+                    }  }
+                }
+            }
+        
+}
+
