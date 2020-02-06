d314b412f9f0401f139d7c84af4a427e5966f830
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-02-06 12:47:33 +0530

    [JD]Add.InventoryManagement programes added to the remote repository by creating a ranch name as ObjectOrientedProgrems

---
+++ ObjectOrientedPrograms/InventoryDataManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ObjectOrientedProgramming
+{
+    class InventoryDataManagement
+    {
+        public class RootObject
+        {
+            public string name { get; set; }
+            public string weight { get; set; }
+            public string price { get; set; }
+        }
+
+        public static void Inventory()
+        {
+            try
+            {
+                string st = File.ReadAllText(@"C:\Users\Neelabh\source\repos\ObjectOrientedProgramming\ObjectOrientedProgramming\ResultBook.json");
+                var res = JsonConvert.DeserializeObject<List<RootObject>>(st);
+                int count = 1;
+                foreach (var f in res)
+                {
+
+                    Console.WriteLine("properties of the {0} item", count);
+                    Console.WriteLine("name : "+f.name);
+                    Console.WriteLine("price : "+f.price);
+                    Console.WriteLine("weight : "+f.weight);
+                    count++;
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine("LOL ...{0}",e);
+            }
+            Console.Read();
+        }
+    }
+}
